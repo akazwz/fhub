390a9758a9e0d0fe2e804882b428f058c3b84374
akazwz <akazwz@pm.me>
2022-03-22 14:25:10 +0800

    add get magnet info

--- api/v1/download.go
+++ api/v1/download.go
@@ -10,6 +10,8 @@ import (
 
 // DownloadByMagnet 根据magnet 下载
 func DownloadByMagnet(c *gin.Context) {
+	magnetLink := c.Query("magnet")
+	log.Println(magnetLink)
 	tClient, err := torrent.NewClient(nil)
 	defer tClient.Close()
 	if err != nil {
@@ -17,7 +19,7 @@ func DownloadByMagnet(c *gin.Context) {
 		response.BadRequest(4000, "new torrent client error", c)
 		return
 	}
-	magnet, err := tClient.AddMagnet("magnet:?xt=urn:btih:ZOCMZQIPFFW7OLLMIC5HUB6BPCSDEOQU")
+	magnet, err := tClient.AddMagnet(magnetLink)
 	if err != nil {
 		log.Println(err)
 		response.BadRequest(4000, "add magnet error", c)
@@ -25,6 +27,7 @@ func DownloadByMagnet(c *gin.Context) {
 	}
 	<-magnet.GotInfo()
 	info := magnet.Info()
+	log.Println(info)
 	response.Ok(2000, info, "get meta info success", c)
 	return
 }
